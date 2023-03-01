5611be98e5a8b5b953884b602b0c6324e9fc3c04
include9it <eduards.butorins@saltpay.co>
2023-03-01 14:09:39 +0200

    split all main processes to logical blocks

--- src/main/java/io/saltpay/Main.java
+++ src/main/java/io/saltpay/Main.java
@@ -1,10 +1,10 @@
 package io.saltpay;
 
 import io.saltpay.model.SsnData;
-import io.saltpay.scrapper.CreditInfo;
 import io.saltpay.scrapper.CreditInfoDataCollector;
-import io.saltpay.steps.StepController;
+import io.saltpay.scrapper.CreditInfoScrapper;
 import io.saltpay.support.DriverManager;
+import io.saltpay.threads.CreditInfoDataCollectorThreadManager;
 import io.saltpay.utils.*;
 
 import java.io.IOException;
@@ -15,40 +15,56 @@ public class Main {
     public static void main(String[] args) throws IOException {
         DriverManager driverManager = new DriverManager();
 
-        StepController stepController = new StepController(driverManager.createChromeDriver());
+//        basicCollectCreditInfo(driverManager);
+
+//        basicCollectPhoneNumbers();
+
+        multiThreadCollectCreditInfo(driverManager);
+    }
+
+    private static void basicCollectCreditInfo(DriverManager driverManager) throws IOException {
+        // Prepare list of input SSN numbers for data collection
         CreditInfoSaveManager ciSaveManager = new CreditInfoSaveManager();
-
         CreditInfoSsnManager creditInfoSsnManager = new CreditInfoSsnManager();
-
         List<String> listOfSsn = creditInfoSsnManager.prepareSsnStartData(ciSaveManager);
 
-        CreditInfo creditInfoScrapper = new CreditInfo(stepController);
-
+        // Start data collection process
+        CreditInfoScrapper creditInfoScrapper = new CreditInfoScrapper(driverManager);
         CreditInfoDataCollector creditInfoDataCollector = new CreditInfoDataCollector(
                 creditInfoScrapper,
                 listOfSsn,
                 ciSaveManager
-        );  // 15 requests per 1 min // Approximately 4 hours, 59 minutes
+        );
+        // 15 requests per 1 min // For 4492 records will be approximately 4 hours, 59 minutes
         creditInfoDataCollector.start();
 
+        // Prepare Excel file
         List<SsnData> savedSsnData = ciSaveManager.readSavedSsnData();
-
 //        List<SsnData> savedSsnChunk = savedSsnData.subList(1743, 3071);
         SaltLogger.basic("savedSsnData size: " + savedSsnData.size());
-
         creditInfoSsnManager.prepareExcelWithSsnData(savedSsnData);
+    }
 
+    private static void multiThreadCollectCreditInfo(DriverManager driverManager) throws IOException {
+        // Prepare list of input SSN numbers for data collection
+        CreditInfoSaveManager ciSaveManager = new CreditInfoSaveManager();
+        CreditInfoSsnManager creditInfoSsnManager = new CreditInfoSsnManager();
+        List<String> listOfSsn = creditInfoSsnManager.prepareSsnStartData(ciSaveManager);
 
+        // Start multi thread collecting info process
+        List<SsnData> multiThreadSsnDataList = CreditInfoDataCollectorThreadManager.start(10, listOfSsn, driverManager);
+
+        ciSaveManager.saveSsnThreadData(multiThreadSsnDataList);
+
+        List<SsnData> savedThreadSsnData = ciSaveManager.readSavedThreadSsnData();
+
+        SaltLogger.basic("savedSsnThreadData size: " + savedThreadSsnData.size());
+
+        creditInfoSsnManager.prepareExcelWithSsnData(savedThreadSsnData);
+    }
+
+    private static void basicCollectPhoneNumbers() {
 //        JaPhoneNumber jaPhoneNumber = new JaPhoneNumber(stepsManager);
 //        jaPhoneNumber.start();
-
-
-//        // Start multi thread collecting info process
-//        List<Task> listOfTasks = new ArrayList<>();
-//        listOfTasks.add(new ScrapperTask(creditInfoScrapper));
-//
-//        ThreadManager.start(10, listOfTasks);
-
-//        creditInfoSsnManager.prepareExcelWithSsnData(creditInfoScrapper.getListOfSsnData());
     }
 }
