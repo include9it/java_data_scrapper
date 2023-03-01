8345c89c49e47b790f401b708dc5e4e9dad6d024
include9it <eduards.butorins@saltpay.co>
2023-03-01 16:57:04 +0200

    removed duplication, code refactor

--- src/main/java/io/saltpay/utils/CreditInfoSaveManager.java
+++ src/main/java/io/saltpay/utils/CreditInfoSaveManager.java
@@ -4,17 +4,11 @@ import io.saltpay.model.SsnData;
 
 import java.util.List;
 
-import static io.saltpay.utils.Constants.CREDIT_INFO_DATA_THREAD_FILE;
-import static io.saltpay.utils.Constants.CREDIT_INFO_DATA_FILE;
-
 public class CreditInfoSaveManager {
 
     private Boolean hasSavedData = null;
-    private Boolean hasSavedThreadData = null;
 
-    public void saveSsnData(SsnData ssnData) {
-        String fileName = CREDIT_INFO_DATA_FILE;
-
+    public void saveSsnData(String fileName, SsnData ssnData) {
         if (checkHasSavedData(fileName)) {
             FileManager.appendModelToFile(fileName, ssnData);
 
@@ -26,9 +20,19 @@ public class CreditInfoSaveManager {
         hasSavedData = true;
     }
 
-    public List<SsnData> readSavedSsnData() {
-        String fileName = CREDIT_INFO_DATA_FILE;
+    public void saveSsnData(String fileName, List<SsnData> ssnDataList) {
+        if (checkHasSavedData(fileName)) {
+            FileManager.appendModelToFile(fileName, ssnDataList);
 
+            return;
+        }
+
+        FileManager.writeModelToFile(fileName, ssnDataList);
+
+        hasSavedData = true;
+    }
+
+    public List<SsnData> readSavedSsnData(String fileName) {
         if (checkHasSavedData(fileName)) {
             return FileManager.readModelsFromFile(fileName);
         }
@@ -42,42 +46,5 @@ public class CreditInfoSaveManager {
         }
 
         return hasSavedData;
-    }
-
-    //
-    //
-    ///
-    // Backup file IO logic for threads test
-
-    public void saveSsnThreadData(List<SsnData> ssnDataList) {
-        String fileName = CREDIT_INFO_DATA_THREAD_FILE;
-
-        if (checkHasSavedThreadData(fileName)) {
-            FileManager.appendModelToFile(fileName, ssnDataList);
-
-            return;
-        }
-
-        FileManager.writeModelToFile(fileName, ssnDataList);
-
-        hasSavedThreadData = true;
-    }
-
-    public List<SsnData> readSavedThreadSsnData() {
-        String fileName = CREDIT_INFO_DATA_THREAD_FILE;
-
-        if (checkHasSavedThreadData(fileName)) {
-            return FileManager.readModelsFromFile(fileName);
-        }
-
-        return null;
-    }
-
-    private boolean checkHasSavedThreadData(String fileName) {
-        if (hasSavedThreadData == null) {
-            hasSavedThreadData = FileManager.readModelsFromFile(fileName) != null;
-        }
-
-        return hasSavedThreadData;
     }
 }
