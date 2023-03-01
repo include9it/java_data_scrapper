c61c61ba06f0c3f32a847ad1d6ac9be2bd8118ab
include9it <eduards.butorins@saltpay.co>
2023-03-01 16:57:37 +0200

    filtered constants

--- src/main/java/io/saltpay/utils/Constants.java
+++ src/main/java/io/saltpay/utils/Constants.java
@@ -6,13 +6,15 @@ public class Constants {
 
     public static String RESOURCE_FILE_PATH = "src/main/resources/";
 
+    // CreditInfo Island company registry
+    public static String CREDIT_INFO_LINK = "https://fyrirtaeki.creditinfo.is";
     public static String CREDIT_INFO_READ_FILE = "ISL Missing Authorizers.xlsx";
     public static String CREDIT_INFO_WRITE_FILE = "ProcuratorList.xlsx";
-    public static String CREDIT_INFO_LINK = "https://fyrirtaeki.creditinfo.is";
     public static String CREDIT_INFO_COMPANY_REGISTRY = "/financeInfo/subjectOverview/%s/companyRegistration";
-    public static String CREDIT_INFO_DATA_FILE = "saved_ssn_list";
-    public static String CREDIT_INFO_DATA_THREAD_FILE = "saved_ssn_list_thread";
+    public static String CREDIT_INFO_BACKUP_FILE = "ssn_list_backup";
     public static String DATA_MODEL_FILE_EXTENSION = ".dat";
 
+    // Ja Phone Island phone registry
     public static String JA_PHONE_REGISTRY_LINK = "https://ja.is";
+    public static String JA_PHONE_BACKUP_FILE = "phone_list_by_ssn_backup";
 }
