e4867a2120aafd112f9554a04523c8fe74236baa
include9it <eduards.butorins@saltpay.co>
2023-03-02 13:58:20 +0200

    refactored thread bot and task

---
+++ src/main/java/io/saltpay/robot/JaPhoneThreadBot.java
@@ -0,0 +1,47 @@
+package io.saltpay.robot;
+
+import io.saltpay.model.ProcuratorPhones;
+import io.saltpay.model.SsnData;
+import io.saltpay.model.chunk.SsnDataChunk;
+import io.saltpay.support.DriverManager;
+import io.saltpay.tasks.JaPhoneCallableTask;
+import io.saltpay.utils.ChunkUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class JaPhoneThreadBot {
+    public static List<ProcuratorPhones> start(int numberOfThreads, List<SsnData> listOfSsn, DriverManager driverManager) {
+        // Create a thread pool with numberOfThreads
+        ExecutorService executor = Executors.newFixedThreadPool(numberOfThreads);
+
+        List<ProcuratorPhones> procuratorPhonesList = new ArrayList<>();
+        List<Future<List<ProcuratorPhones>>> futuresOfProcuratorPhones = new ArrayList<>();
+
+        List<SsnDataChunk> listOfSsnDataChunks = ChunkUtil.prepareSsnDataChunks(numberOfThreads, listOfSsn);
+
+        // Submit tasks to the executor
+        listOfSsnDataChunks.forEach(chunk -> {
+            Callable<List<ProcuratorPhones>> callable = new JaPhoneCallableTask(chunk.hashCode(), driverManager, chunk);
+
+            Future<List<ProcuratorPhones>> future = executor.submit(callable);
+
+            futuresOfProcuratorPhones.add(future);
+        });
+
+        // Get ssn data from futures
+        futuresOfProcuratorPhones.forEach(phonesListFuture -> {
+            try {
+                procuratorPhonesList.addAll(phonesListFuture.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        // Shutdown the executor when all tasks are completed
+        executor.shutdown();
+
+        return procuratorPhonesList;
+    }
+}
---
+++ src/main/java/io/saltpay/tasks/JaPhoneCallableTask.java
@@ -0,0 +1,49 @@
+package io.saltpay.tasks;
+
+import io.saltpay.model.ProcuratorPhones;
+import io.saltpay.model.chunk.SsnDataChunk;
+import io.saltpay.scrapper.JaPhoneNumberScrapper;
+import io.saltpay.support.DriverManager;
+import io.saltpay.utils.SaltLogger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class JaPhoneCallableTask implements Callable<List<ProcuratorPhones>> {
+
+    private final int threadId;
+    private final JaPhoneNumberScrapper jaPhoneNumberScrapper;
+
+    private final SsnDataChunk ssnDataChunk;
+
+    private final List<ProcuratorPhones> listOfProcuratorPhones = new ArrayList<>();
+
+    public JaPhoneCallableTask(int threadId, DriverManager driverManager, SsnDataChunk ssnDataChunk) {
+        this.threadId = threadId;
+        this.jaPhoneNumberScrapper = new JaPhoneNumberScrapper(driverManager);
+        this.ssnDataChunk = ssnDataChunk;
+    }
+
+    @Override
+    public List<ProcuratorPhones> call() throws Exception {
+        SaltLogger.basic("Thread -> " + threadId);
+
+        jaPhoneNumberScrapper.enterWebsite();
+
+        // Get Procurator phone numbers by Full Name
+        ssnDataChunk.getListOfSsnData().forEach(ssnData ->
+                ssnData.getListOfProcurator().forEach(procurator -> {
+                    SaltLogger.basic("Thread -> " + threadId);
+
+                    ProcuratorPhones procuratorPhones = jaPhoneNumberScrapper.findAndCollectDataByFullName(
+                            procurator.getFullName()
+                    );
+
+                    listOfProcuratorPhones.add(procuratorPhones);
+                })
+        );
+
+        return listOfProcuratorPhones;
+    }
+}
